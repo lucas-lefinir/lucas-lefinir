f0dff600efb9cbdc28edc62eec4999b81360db0b
Lucas Lefinir <lucas2.lefinir@gmail.com>
2019-12-24 16:10:40 -0300

    adding sound and image effects

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 function message(){
     alert("sumbudrule");
 }
+
+function playAudio(){
+    var p = document.getElementById("myPicture");
+    var x = document.getElementById("myAudio");
+    x.play();
+    p.width = 240;
+    p.height = 240;
+
+}
